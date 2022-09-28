c5c9420852e14c1f05e1d20aca63dcea4a22a52f
Igor Nascimento Costa <igor.costa@turbopartners.com.br>
2022-09-28 14:03:00 -0300

    Added aula 7

---
+++ aula07.java
@@ -0,0 +1,11 @@
+public class aula07 {
+    public static void main(String[] args) {
+        String nome = "Let's Code";
+        saudacao(nome);
+
+    }
+
+    public static void saudacao(String nome) {
+        System.out.println("Hello, " + nome);
+    }
+}
