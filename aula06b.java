7bdb0da8c163073a6de70ceac98f18ddd3df1a54
Igor Nascimento Costa <igor.costa@turbopartners.com.br>
2022-09-28 14:02:53 -0300

    Added aula 6b

---
+++ aula06b.java
@@ -0,0 +1,24 @@
+public class aula06b {
+    public static void main(String[] args) {
+        int[] numeros = { 9, 10, 12, 25, 2 };
+        int maior = numeros[0];
+        int menor = numeros[0];
+        float media = 0;
+
+        for (int i = 0 ;i < numeros.length; i++) {
+            if (numeros[i] > maior) {
+                maior = numeros[i];
+            }
+
+            if (numeros[i] < menor) {
+                menor = numeros[i];
+            }
+
+            media += numeros[i];
+        }
+
+        System.out.println("Maior: " + maior);
+        System.out.println("Menor: " + menor);
+        System.out.println("Media: " + (Float) media/numeros.length);
+    }
+}
