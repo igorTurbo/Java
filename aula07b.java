5cfbdffba844ff25f46c075816d18bc99d367334
Igor Nascimento Costa <igor.costa@turbopartners.com.br>
2022-09-28 14:03:06 -0300

    Added aula 7b

---
+++ aula07b.java
@@ -0,0 +1,11 @@
+public class aula07b {
+    public static void main(String[] args) {
+        int result = soma(2, 3);
+
+        System.out.println(result);
+    }
+
+    public static int soma(int a ,int b) {
+        return a + b;
+    }
+}
